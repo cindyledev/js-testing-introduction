61fdedba08dbabc7f9b1b73fc2f1ee0df7a77499
Cindy Le <cindyledev@gmail.com>
2020-12-08 11:28:59 -0500

    add data-less text test

--- test/util.test.js
+++ test/util.test.js
@@ -5,3 +5,8 @@ test('should output name and age', () => {
   const text = generateText('Cindy', 28);
   expect(text).toBe('Cindy (28 years old)');
 });
+
+test('should output data-less text', () => {
+  const text = generateText('', null);
+  expect(text).toBe(' (null years old)');
+})
